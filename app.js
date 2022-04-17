e810ec8dbdfc31a7a445bf28902ffe6a3eddff7e
ahmadhelwa <AHM0173377@ju.edu.jo>
2022-04-18 02:12:06 +0300

    i modified the task

--- app.js
+++ app.js
@@ -3,34 +3,55 @@ var username =  prompt  (" Enter your name ");
 
 var gender = prompt  (" Enter male or female");
 
+
 if (gender.toLowerCase()  == "male")
 
 {
     var age = prompt  (" Enter age "); 
 
-{
-    (age > 0)? confirm(` Hello MR ${username}`): alert (` Enter a valid number !`)
-}
+
+    if (age > 0)
+    { 
+        var message =  confirm("if  you need watch message welcome ")
+      {  (message == true) ? alert(` Hello Mr ${username} your age is ${age}`): ` 0 `;}
+    }
+        else alert (` The age entered is incorrect, please try again and write your age using the numbers and the number is greater than 0  `)
+
 
 }
+    
 
-else if(gender.toLowerCase() == "female")
+
+else if (gender.toLowerCase()  == "female")
+
 {
     var age = prompt  (" Enter age "); 
-   {
-    (age > 0)? confirm(` Hello Ms ${username}`): alert (` Enter a valid number !`)
-   }
+
+
+    if (age > 0)
+    { 
+        var message =  confirm(" if you need watch message welcome ")
+      {  (message == true) ? alert(` Hello Ms ${username} your age is ${age}`): ` 0 `;}
+    }
+        else alert (` The age entered is incorrect, please try again and write your age using the numbers and the number is greater than 0  `)
+
 
 }
 
 else
-
-{ var age = prompt  (" Enter age "); 
+{
+    var age = prompt  (" Enter age "); 
 
 {
-    (age > 0)? confirm(` Hello  ${username}`): alert (` Enter a valid number !`)
-}
+    if (age > 0)
+    { 
+        var message =  confirm(" you need watch message welcome ")
+      {  (message == true) ? alert(` Hello  ${username} your age is ${age}`): ` 0 `;}
+    }
+        else alert (` The age entered is incorrect, please try again and write your age using the numbers and the number is greater than 0    !`)
 
 }
+}
+
 
 
