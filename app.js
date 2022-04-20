60d92cd30c46bc3cbd0a8fd729008cf34af096cb
ahmadhelwa <AHM0173377@ju.edu.jo>
2022-04-20 03:51:13 +0300

    updeat fill js

--- app.js
+++ app.js
@@ -52,6 +52,65 @@ else
 
 }
 }
+{
+let answerPrompt1 =  prompt(" you need add to   js  (yes/no) ");
+let answerPrompt2  = prompt(" you need add to   c   (yes/no) ");
+let answerPrompt3  = prompt(" you need add to  c++ ( yes/no) ");
+let arrayn = [] ;
+arrayn.push(gitprom());
+arrayn.push(gitprom1());
+arrayn.push(gitprom2());
+for(let i = 0 ; i < arrayn.length ; i++ )
+{
+
+  console.log(arrayn[i]);
+
+}
+function gitprom ()
+{
+  if (answerPrompt1 == "" || answerPrompt1 == null )
+{
+  answerPrompt1 ="undfind";
+  return answerPrompt1;
+}
+
+else if(`${answerPrompt1.toLowerCase()  == "yes" }` || `${answerPrompt1.toLowerCase()  == "no" }`)
+{
+  return answerPrompt1;
+}
+
+}
+
+function gitprom1 ()
+{
+  if (answerPrompt2 == "" || answerPrompt2 == null )
+{
+  answerPrompt2 ="undfind";
+  return answerPrompt2;
+}
+
+else if(`${answerPrompt2.toLowerCase()  == "yes" }` || `${answerPrompt2.toLowerCase()  == "no" }`)
+{
+  return answerPrompt2;
+}
+}
 
 
 
+function gitprom2 ()
+{
+  if (answerPrompt3 == "" || answerPrompt3 == null )
+{
+  answerPrompt3 ="undfind";
+  return answerPrompt3;
+}
+
+
+else if(`${answerPrompt3.toLowerCase()  == "yes" }` || `${answerPrompt3.toLowerCase()  == "no" }`)
+{
+  return answerPrompt3;
+}
+
+}
+
+}
